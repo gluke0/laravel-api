9fa340d3e095311047d73e5b92806cb6fa4e11d6
gluke0 <milanesigianluca@gmail.com>
2023-06-28 16:22:55 +0200

    added the paginate() method

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -11,7 +11,10 @@ class ProjectController extends Controller
     public function index(){
 
         // with and get allow me to extract all the data, bewtween brackets, from the db including relationships
-        $projects = Project::with('category', 'technologies')->get();;
+        // $projects = Project::with('category', 'technologies')->get();
+
+        // Laravel helps us with pages, now it separates content in diff pages, 5 projects per page
+        $projects = Project::with('category', 'technologies')->paginate(5);
 
         return response()->json([
             'success' => true,
