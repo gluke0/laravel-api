0ee2d59f0eda88935d36e96c8ee1ef40ce70616c
gluke0 <milanesigianluca@gmail.com>
2023-06-30 16:45:03 +0200

    pub function categorycontroller

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -4,8 +4,17 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Admin\Category;
 
 class CategoryController extends Controller
 {
-    //
+    public function index(){
+        $categories = Category::All();
+        return response()->json(
+            [
+                'success' => true,
+                'categories' => $categories
+            ],
+        );
+    }
 }
