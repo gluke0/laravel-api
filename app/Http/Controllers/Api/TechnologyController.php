95fee48806d9d030c725387a97f677c3cd689f5f
gluke0 <milanesigianluca@gmail.com>
2023-07-03 11:28:10 +0200

    created technologyController

---
+++ app/Http/Controllers/Api/TechnologyController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Admin\Technology;
+use Illuminate\Http\Request;
+
+class TechnologyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+}
